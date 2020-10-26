4717c17ea22294e8f05e1ba92aaed2a1df475c25
NICOLAS MONTAGUT CHAPARRO <nmontagut@ucundinamarca.edu.co>
2020-10-25 23:25:54 -0500

    Modificacion del formulario y creacion del SelectOneMenu donde persiste el error en el llamado del dato recogido

--- src/main/java/co/edu/unicundi/formulario/IndexController.java
+++ src/main/java/co/edu/unicundi/formulario/IndexController.java
@@ -5,8 +5,11 @@
  */
 package co.edu.unicundi.formulario;
 
+import java.util.ArrayList;
+import java.util.List;
 import javax.inject.Named;
 import javax.enterprise.context.RequestScoped;
+import javax.faces.model.SelectItem;
 
 /**
  *
@@ -15,19 +18,52 @@ import javax.enterprise.context.RequestScoped;
 @Named(value = "indexController")
 @RequestScoped
 public class IndexController {
-
-    private String nombre;
+    
+    private String nombre;//cada variable debe tener un metodo get y set
     private String apellido;
-    /**
-     * Creates a new instance of IndexController
-     */
+    private int cedula;
+    
+    //private int nacimiento;
+    private String estudios;
+    private List<SelectItem> listaEstudios;
+    
+    
     public IndexController() {
+        // aca se inicializan las variables 
+        //ej: arriba se declara private int cedula;
+        //aca se inicializa this.cedula=0;
+        this.cedula=0; 
     }
 
     public void click (){
-        System.out.println("Nombre :"+ this.nombre);
-        System.out.println("Apellido: " + this.apellido);
+        System.out.println("Cedula:"+ this.cedula);
+        System.out.println("Nombre:"+ this.nombre);
+        System.out.println("Apellido:" + this.apellido);
+        
+        //System.out.println("fecha nacimiento:"+nacimiento); falta declarar, implementar, asignar valor, get y set
+        System.out.println("estudios: "+this.estudios);
+        
     }
+    
+    public void listaestudios(){
+        listaEstudios = new ArrayList<SelectItem>();
+	listaEstudios.add(new SelectItem(1, "Primaria"));
+	listaEstudios.add(new SelectItem(2, "Secundaria"));
+	listaEstudios.add(new SelectItem(3, "Tecnico"));
+	listaEstudios.add(new SelectItem(4, "Tecnologo"));
+	listaEstudios.add(new SelectItem(5, "Profsional"));
+        listaEstudios.add(new SelectItem(6, "Magister"));
+    }
+    
+    
+    public int getCedula() {
+        return cedula;
+    }
+
+    public void setCedula(int cedula) {
+        this.cedula = cedula;
+    }
+    
     public String getNombre() {
         return nombre;
     }
@@ -42,6 +78,22 @@ public class IndexController {
 
     public void setApellido(String apellido) {
         this.apellido = apellido;
+    }
+
+    public String getEstudios() {
+        return estudios;
+    }
+
+    public void setEstudios(String estudios) {
+        this.estudios = estudios;
+    }
+
+    public List<SelectItem> getListaEstudios() {
+        return listaEstudios;
+    }
+
+    public void setListaEstudios(List<SelectItem> listaEstudios) {
+        this.listaEstudios = listaEstudios;
     }
     
 }
